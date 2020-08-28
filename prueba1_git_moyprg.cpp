d1da044a37c185e5ca117485ee265c632c11aa59
moyprg <moy30spl@gmail.com>
2020-08-28 01:59:29 -0500

    Se cambio el commint(index)

--- prueba1_git_moyprg.cpp
+++ prueba1_git_moyprg.cpp
@@ -11,6 +11,7 @@ int main(){
 	cout<<"Hola mundo, esto es una prube de git."<<endl;
 	cout<<"Para esta prueba se vio un curso en udemy."<<endl;
 	cout<<"Se agregan mas lineas de trabajo."<<endl;
+	cout<<"Se modifica un segundo commit."<<endl;
 	
 	return 0;
 }
