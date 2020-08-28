db2e76cf70411eca045d240f27ed5862083a4f7d
moyprg <moy30spl@gmail.com>
2020-08-28 01:49:16 -0500

    se agregaron lineas

--- prueba1_git_moyprg.cpp
+++ prueba1_git_moyprg.cpp
@@ -8,7 +8,9 @@ using namespace std;
 
 int main(){
 	
-	cout<<"Hola mundo, esto es una prube de git"<<endl;
+	cout<<"Hola mundo, esto es una prube de git."<<endl;
+	cout<<"Para esta prueba se vio un curso en udemy."<<endl;
+	cout<<"Se agregan mas lineas de trabajo."<<endl;
 	
 	return 0;
 }
