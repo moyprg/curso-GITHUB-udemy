30059dbe6de3a5d18a8b9d22c6d192c8b422326c
moyprg <moy30spl@gmail.com>
2020-09-02 00:47:15 -0500

    Prueba ignore

---
+++ prueba1_git_moyprg - copia.cpp
@@ -0,0 +1,23 @@
+
+
+//prueba c++ git moyprg
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	
+	cout<<"Hola mundo, esto es una prube de git."<<endl;
+	cout<<"Para esta prueba se vio un curso en udemy."<<endl;
+	cout<<"Se agregan mas lineas de trabajo."<<endl;
+	cout<<"Se modifica un segundo commit."<<endl;
+	cout<<"Se agrega mas codigo."<<endl;
+	cout<<"Esto esta en la rama moy2."<<endl;
+	cout<<"Se unieron las ramas con el comando merge"<<endl;
+	cout<<"Se ha realizo la prueba de subir el archivo al repositorio de github"<<endl;
+	
+	return 0;
+}
+
+
